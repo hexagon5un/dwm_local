39f606119401d60a9e38895946b330ade3265a7f
Elliot Williams <elliot@pre.postero.us>
2014-09-06 22:32:40 +0200

    backed out bstack, applied single-tagset
    makes xinerama window-switching behave like xmonad
    i.e. one set of tags across both monitors
    (pull up edit on either, get all tagged edit)

--- config.def.h
+++ config.def.h
@@ -31,15 +31,11 @@ static const float mfact      = 0.55; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = True; /* True means respect size hints in tiled resizals */
 
-#include "bstack.c"
-#include "bstackhoriz.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-	{ "TTT",      bstack },
-	{ "===",      bstackhoriz },
 };
 
 /* key definitions */
@@ -60,6 +56,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 #include "selfrestart.c"
 
+#include "keepfloatingposition.c"
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
