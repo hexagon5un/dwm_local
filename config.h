5336a6a896974131f0fe5c02577c49a3feafdeee
Elliot Williams <elliot@pre.postero.us>
2014-09-07 00:31:27 +0200

    worked in bstack into the single-tags patch
    essentially changed a bunch of calls adding in which monitor

--- config.h
+++ config.h
@@ -81,15 +81,15 @@ static const float mfact      = 0.75; /* factor of master area size [0.05..0.95]
 static const int nmaster      = 1;    /* number of clients in master area */
 static const Bool resizehints = False; /* True means respect size hints in tiled resizals */
 
-//#include "bstack.c"
-//#include "bstackhoriz.c"
+#include "bstack.c"
+#include "bstackhoriz.c"
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
-//	{ "TTT",      bstack },
-//	{ "===",      bstackhoriz },
+	{ "TTT",      bstack },
+	{ "===",      bstackhoriz },
 };
 
 #include "keepfloatingposition.c"
